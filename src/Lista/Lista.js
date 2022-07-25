4f44409cbff64374d4b7fc889306a6e318d140b3
unknown <diego2002lml@gmail.com>
2022-07-25 15:11:00 -0400

    N3

--- src/Lista/Lista.js
+++ src/Lista/Lista.js
@@ -1,5 +1,7 @@
 import React ,{ useEffect, useState} from "react";
 
+import "./Lista.css"
+
 
 const Lista = (props) => {
 
@@ -21,12 +23,35 @@ const Lista = (props) => {
         return (
             <>
 
-            {pokemon.map((pokemon, key) => {
-                return(<div key={key}>{pokemon.name} {pokemon.weigth}</div>)
-            })}
+            {/** Aca solo me deja mostrar resultados de name y tambien de las url pero no puedo acceder mas alla de los results */}
 
-            
+            <div className="card-container">
 
+                <div className="card-pokemon">
+                 {pokemon.map((pokemon, key) => {
+                 return(<div key={key}>{pokemon.name}</div>)
+                })}
+                </div>
+
+                <div className="img-pokemon">
+                 {pokemon.map((pokemon, key) => {
+                 return(<div key={key}>{pokemon.sprites /**.front_default */}</div>)
+                 })}
+                </div>
+
+                <div className="card-weigth">
+                 {pokemon.map((pokemon, key) => {
+                 return(<div key={key}>{pokemon.weigth}</div>)
+                })}
+                </div>
+
+                <div className="card-ability">
+                {pokemon.map((pokemon, key) =>{
+                    return(<div key={key}>{pokemon.ability}</div>)
+                })}
+                </div>
+
+            </div> 
             
             </>
         )
